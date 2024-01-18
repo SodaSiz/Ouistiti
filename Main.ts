b9d1477a8779985bbcc2db05bd4a2c8237cfbb81
SodaSiz <despairos@proton.me>
2024-01-18 05:02:31 +0100

    Implemented MongoDB and Mongoose to setup a database

--- Main.ts
+++ Main.ts
@@ -16,23 +16,10 @@ const loadHandlers = async () => {
   }));
 };
 
-process.on('exit', (code) => {
-  console.log(`Le processus s'est arrêté avec le code ${code}`);
-});
-
-process.on('uncaughtException', (err, origin) => {
-  console.log(`UNCAUGHT_EXCEPTION: ERROR:\n${err}\n\nORIGIN:\n${origin}`);
-});
-
-process.on('unhandledRejection', (promise, reason) => {
-  console.log(`UNHANDLED_REJECTION: REASON: \n${reason}\n\nPROMISE:\n${promise}`)
-});
-
-process.on('warning', (...args) => {
-  console.log('WARNING:\n', ...args);
-});
-
-console.log(MongoDB_Options);
+process.on('exit', (code) => {console.log(`Le processus s'est arrêté avec le code ${code}`)});
+process.on('uncaughtException', (err, origin) => {console.log(`UNCAUGHT_EXCEPTION: ERROR:\n${err}\n\nORIGIN:\n${origin}`);});
+process.on('unhandledRejection', (promise, reason) => {console.log(`UNHANDLED_REJECTION: REASON: \n${reason}\n\nPROMISE:\n${promise}`)});
+process.on('warning', (...args) => {console.log('WARNING:\n', ...args)});
 
 if (process.env.MONGODB_URI && MongoDB_Options) {
   mongoose.connect(process.env.MONGODB_URI, MongoDB_Options).then(() => {console.log('Le client est connecté à la base de donnée')}).catch(err => {console.log(err);});
